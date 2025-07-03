1fe3ea962f12f70201225638aa82c654a37f8cb2
Tharinda Pathirana <tharindapathirana27@gmail.com>
2025-07-03 14:30:33 +0530

    changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ export default function Home() {
 
     const loadUsers= async() => {
         const result = await axios.get("http://localhost:8080/users")
-        console.log(result.data);
+        setUsers(result.data);
     };
 
 
@@ -21,31 +21,26 @@ export default function Home() {
             <table className="table table-hover border shadow">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">ID</th>
+      <th scope="col">Name</th>
+      <th scope="col">Username</th>
+      <th scope="col">Email</th>
+      <th scope="col">Action</th>
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>John</td>
-      <td>Doe</td>
-      <td>@social</td>
-    </tr>
+
+    {
+        users.map((user,index)=>(
+        <tr key={index}>
+            <th scope="row">{index+1}</th>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>{user.email}</td>
+        </tr>
+        ))
+    }
+    
   </tbody>
 </table>
       
