bce02157871a093a5f87f91189ab7985bd60e0d1
Tharinda Pathirana <tharindapathirana27@gmail.com>
2025-07-08 13:57:30 +0530

    add view,edit,delete

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -37,6 +37,11 @@ export default function Home() {
             <td>{user.name}</td>
             <td>{user.username}</td>
             <td>{user.email}</td>
+            <td>
+              <button className="btn btn-success mx-2">View</button>
+              <button className="btn btn-outline-secondary mx-2">Edit</button>
+              <button className="btn btn-danger mx-2">Delete</button>
+            </td>
         </tr>
         ))
     }
